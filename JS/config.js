a386559d7e87918e853f9b361b27ca07912f9dc3
mackv42 <mackv42@gmail.com>
2020-12-07 22:49:30 -0600

    add config.js

---
+++ JS/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_i0CxQ41pf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1nl3m422sajcm2n6h2ren9d00b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
